806ca488a79158b7a97c574a02c338844174729d
Bing Xie <bing708@gmail.com>
2015-09-30 19:51:04 +1000

    responsive layout

--- js/app.js
+++ js/app.js
@@ -7,3 +7,10 @@ function makeCartScrollNicely() {
   var cart = document.querySelector('.cart');
   Ps.initialize(cart);
 }
+
+var $toggle = document.querySelector(".site__right-sidebar-toggle");
+
+$toggle.addEventListener("click",function() {
+  console.log('toggle sidebar');
+  document.body.classList.toggle("js-show-right-sidebar");
+});
